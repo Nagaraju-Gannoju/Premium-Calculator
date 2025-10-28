04d16925cb5052d7c47532813b9f927961159508
Nagaraju-Gannoju <nag.sdnr@gmail.com>
2025-10-28 11:51:36 +0530

    Rating Values

--- premium-calculator/src/app/premium-calculator/premium-calculator.component.ts
+++ premium-calculator/src/app/premium-calculator/premium-calculator.component.ts
@@ -34,10 +34,10 @@ constructor(private fb: FormBuilder) {
 ];
 
 ratingFactors: { [key: string]: number } = {
-  'Professional': 1.0,
-  'White Collar': 1.25,
-  'Light Manual': 1.5,
-  'Heavy Manual': 1.75
+  'Professional': 1.5,
+  'White Collar': 2.25,
+  'Light Manual': 11.50,
+  'Heavy Manual': 31.75
 };
 
 
